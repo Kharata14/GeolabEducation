bea095b9a17bafe4b261ebba3f0d3a00c07a1db8
Nika Kharatishvili <daimlernika@gmail.com>
2024-09-22 20:36:33 +0400

    Number check (positive-negative)

--- GeolabEducation/Program.cs
+++ GeolabEducation/Program.cs
@@ -1,3 +1,18 @@
 ﻿
+//1. შემოწმება, რიცხვი დადებითია თუ უარყოფითი
 
+Console.WriteLine("Enter Number");
+int number = int.Parse(Console.ReadLine());
 
+if (number > 0)
+{
+    Console.WriteLine("Number is positive");
+}
+else if (number == 0)
+{
+    Console.WriteLine("Number is zero");
+}
+else
+{
+    Console.WriteLine("Numebr is Negative");
+}
