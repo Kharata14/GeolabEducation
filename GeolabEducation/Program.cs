860e726fc4433e0fe888700fdfa543a6f11f9990
Nika Kharatishvili <daimlernika@gmail.com>
2024-09-22 22:34:42 +0400

    check if leap year

--- GeolabEducation/Program.cs
+++ GeolabEducation/Program.cs
@@ -1,3 +1,15 @@
 ﻿
+//4. ნაკიანი წლის შემოწმება
 
+Console.WriteLine("Enter year");
 
+int year = int.Parse(Console.ReadLine());
+
+if (year % 4 == 0 && year % 100 != 0 && year % 400 != 0)
+{
+    Console.WriteLine("Leap year");
+}
+else
+{
+    Console.WriteLine("Normal year");
+}
