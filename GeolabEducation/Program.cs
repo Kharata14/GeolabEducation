9dbc0af317b016659193424b751107e6fb36830a
Nika Kharatishvili <daimlernika@gmail.com>
2024-09-22 20:56:40 +0400

    Number Check (even-odd)

--- GeolabEducation/Program.cs
+++ GeolabEducation/Program.cs
@@ -1,6 +1,7 @@
 ﻿
 //1. შემოწმება, რიცხვი დადებითია თუ უარყოფითი
 
+/*
 Console.WriteLine("Enter Number");
 int number = int.Parse(Console.ReadLine());
 
@@ -15,4 +16,23 @@ else if (number == 0)
 else
 {
     Console.WriteLine("Numebr is Negative");
+}
+
+*/
+
+
+
+//2. შემოწმება, რიცხვი ლუწია თუ კენტი
+
+Console.WriteLine("Enter Number");
+
+int number = int.Parse(Console.ReadLine());
+
+if (number % 2 == 0)
+{
+    Console.WriteLine("Number is even");
+}
+else
+{
+    Console.WriteLine("Number is odd");
 }
