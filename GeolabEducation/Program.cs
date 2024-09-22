946dca1fd0170d2095e2fb9cc716125a25685802
Nika Kharatishvili <daimlernika@gmail.com>
2024-09-22 22:49:54 +0400

    Password check

--- GeolabEducation/Program.cs
+++ GeolabEducation/Program.cs
@@ -1,3 +1,17 @@
 ﻿
 
+Console.WriteLine("Enter Password");
+
+string password = "secret123";
+
+string passwordCheck = Console.ReadLine();
+
+if (passwordCheck == password)
+{
+    Console.WriteLine("Access Granted");
+}
+else
+{
+    Console.WriteLine("Access Forbidden");
+}
 
