80660ce162ec67c65ef8d56104a0a75d4590bc0b
Nika Kharatishvili <daimlernika@gmail.com>
2024-09-24 13:00:41 +0400

    Buy depends on age

--- GeolabEducation/Program.cs
+++ GeolabEducation/Program.cs
@@ -1,3 +1,18 @@
 ﻿
+Console.WriteLine("Enter country");
+string country = Console.ReadLine();
+Console.WriteLine("Enter age");
+int age = int.Parse(Console.ReadLine());
 
-
+if (country == "usa" || country == "USA" || country == "Usa" && age >= 21)
+{
+    Console.WriteLine("Purchase is allowed");
+}
+else if (country != "usa" || country == "USA" || country == "Usa" && age >= 18)
+{
+    Console.WriteLine("Purchase is allowed");
+}
+else
+{
+    Console.WriteLine("Purchase is prohibited");
+}
